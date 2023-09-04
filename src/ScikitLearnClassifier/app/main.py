7f363a61062f7ebbee2db59decf4521359fae569
AntonIV8 <anton-iv87@yandex.ru>
2023-09-04 16:27:17 +0500

    Добавлен питон-проект ScikitLearnClassifier с обработкой POST-запроса. Docker для него

---
+++ src/ScikitLearnClassifier/app/main.py
@@ -0,0 +1,12 @@
+from typing import Union
+from fastapi import FastAPI
+from .entities.request_entities import HttpTextDocument
+
+app = FastAPI()
+
+@app.post("/classify", 
+          summary="Takes a document docx file and classifies it according the classifier",
+          response_description="A JSON response in format {'document_name':name, 'text': text, 'class_id': ID}"
+         )
+async def classify(textDocument: HttpTextDocument):
+    return {'document_name': textDocument.name, 'text': textDocument.text, 'class_id': 'TEST'}
