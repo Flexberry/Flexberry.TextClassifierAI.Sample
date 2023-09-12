90d65c605551872f374ee9fd5fc025e2d73c2981
Ruslan Bannikov <bannikov.ru@inbox.ru>
2023-09-12 09:17:27 +0500

    feat: добавлен запрос на классификацию текста и его обработка

--- src/ScikitLearnClassifier/app/main.py
+++ src/ScikitLearnClassifier/app/main.py
@@ -1,12 +1,25 @@
-from typing import Union
-from fastapi import FastAPI
-from .entities.request_entities import HttpTextDocument
+import uvicorn
+from fastapi import FastAPI, HTTPException
+
+from entities.request_entities import HttpTextDocument
+from app.model.classifier import Classifier
 
 app = FastAPI()
 
-@app.post("/classify", 
-          summary="Takes a document docx file and classifies it according the classifier",
-          response_description="A JSON response in format {'document_name':name, 'text': text, 'class_id': ID}"
-         )
-async def classify(textDocument: HttpTextDocument):
-    return {'document_name': textDocument.name, 'text': textDocument.text, 'class_id': 'TEST'}
+
+@app.post("/classify",
+          summary="Takes a string and classifies it according the classifier",
+          response_description="A JSON response in format {'text_category': category}"
+          )
+async def classify(text_document: HttpTextDocument):
+    try:
+        classifier = Classifier()
+        category = classifier.classified_text(text_document.text)
+    except Exception as ex:
+        raise HTTPException(status_code=400, detail=str(ex))
+
+    return {'text_category': category}
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="127.0.0.1", port=8000)
