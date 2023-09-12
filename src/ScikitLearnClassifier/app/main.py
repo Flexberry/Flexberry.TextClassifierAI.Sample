93dee3331354eb076438e75df7447d5476126aae
Ruslan Bannikov <bannikov.ru@inbox.ru>
2023-09-12 09:26:00 +0500

    feat: добавлен запрос на обучение новой модели классификатора и её обучение и сохранение в файл.

--- src/ScikitLearnClassifier/app/main.py
+++ src/ScikitLearnClassifier/app/main.py
@@ -1,6 +1,8 @@
-from typing import Union
-from fastapi import FastAPI
-from .entities.request_entities import HttpTextDocument
+import pandas as pd
+from io import StringIO
+
+import uvicorn
+from fastapi import FastAPI, File, HTTPException
 
 app = FastAPI()
 
@@ -10,3 +12,40 @@ app = FastAPI()
          )
 async def classify(textDocument: HttpTextDocument):
     return {'document_name': textDocument.name, 'text': textDocument.text, 'class_id': 'TEST'}
+
+@app.post("/classify",
+          summary="Takes a string and classifies it according the classifier",
+          response_description="A JSON response in format {'text_category': category}"
+          )
+async def classify(text_document: HttpTextDocument):
+    try:
+        classifier = Classifier()
+        category = classifier.classified_text(text_document.text)
+    except Exception as ex:
+        raise HTTPException(status_code=400, detail=str(ex))
+
+    return {'text_category': category}
+
+
+@app.post("/model/new",
+          summary="Create new classifier model",
+          response_description="Information string about successfully classifier model created")
+async def new_model(file_bytes: bytes = File(),
+                    code_page: str = 'windows-1251',
+                    delimiter: str = ';',
+                    x_field_name: str = 'shortcontent',
+                    y_field_name: str = 'actkind'):
+
+    try:
+        data = pd.read_csv(StringIO(file_bytes.decode(code_page)), delimiter=delimiter)
+    except Exception as ex:
+        raise HTTPException(status_code=400, detail=str(ex))
+
+    classifier = Classifier()
+    classifier.new_model(train=data, x_field_name=x_field_name, y_field_name=y_field_name)
+
+    return "Classifier model was successfully created!"
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="127.0.0.1", port=8000)
---
+++ src/ScikitLearnClassifier/app/model/classifier.py
@@ -0,0 +1,87 @@
+import pickle
+import pandas as pd
+from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
+from sklearn.linear_model import LogisticRegression
+
+from sklearn.model_selection import train_test_split
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import StandardScaler
+
+# Объём данных для тестирования из общего количества.
+TEST_SIZE: float = 0.2
+# Значение для генератора случайных чисел при разделении общего количества данных
+# на случайные множества для обучения и тестирования.
+RANDOM_STATE: int = 2
+# Имя файла модели
+MODEL_FILE_NAME: str = "classifier_model.pkl"
+# Список стоп-слов для классификатора
+STOP_WORDS: list = ['english', 'spanish']
+
+
+class Classifier:
+    """
+    Класс с моделью для классифиуации текста по категориям.
+    """
+
+    def new_model(self, train: pd.DataFrame, x_field_name: str, y_field_name: str):
+        """
+        Обучение новой модели классификатора.
+        :param train: Данные для обучения и получения оценки точности обучения.
+        :param x_field_name: Имя поля в train с влияющим параметром на целевое значение.
+        :param y_field_name: Имя поля в train с целевыми значениями.
+        """
+        train = self.filtered_train(train, [x_field_name, y_field_name])
+        y = train[y_field_name]
+        x = train[x_field_name]
+        x_train, x_test, y_train, y_test = train_test_split(
+            x, y,
+            test_size=TEST_SIZE,
+            random_state=RANDOM_STATE)
+
+        model = Pipeline([('vect', CountVectorizer(stop_words=STOP_WORDS)),
+                          ('tfidf', TfidfTransformer()),
+                          ('scale', StandardScaler(with_mean=False)),
+                          ('clf', LogisticRegression(n_jobs=1, class_weight='balanced', C=1e3))])
+        model.fit(x_train, y_train)
+
+        self.save_model(model)
+
+    def filtered_train(self, train: pd.DataFrame, fields: list):
+        """
+        Фильтрация данных.
+        :param train: Данные для фильтрации.
+        :param fields: Список полей, к которым применяются правила фильртации.
+        :return: Отфильтрованные данные.
+        """
+        return train.dropna(subset=fields)
+
+    def save_model(self, model):
+        """
+        Сохранение обученной модели классификатора в файл с именем MODEL_FILE_NAME.
+        :param model: Обученная модель классификатора.
+        """
+        with open(MODEL_FILE_NAME, 'wb') as file:
+            pickle.dump(model, file)
+
+    def load_model(self):
+        """
+        Загрузка ранее обученной модели классификатора из файла с именем MODEL_FILE_NAME.
+        :return: Обученная модель классификатора.
+        """
+        with open(MODEL_FILE_NAME, 'rb') as file:
+            model = pickle.load(file)
+
+        return model
+
+    def classified_text(self, text: str):
+        """
+        Классификация строки текста согласно обученной ранее модели классификатора.
+        :param text: Классифицируемый текст
+        :return: Категория, к которой относится текст
+        """
+        try:
+            model = self.load_model()
+        except FileNotFoundError as ex:
+            raise FileNotFoundError("Classifier model wasn't found!")
+
+        return model.predict([text])[0]
