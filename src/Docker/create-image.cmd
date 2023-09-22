f2ebe8b0e6750c7f8c10fabed963595eb128be05
AntonIV8 <anton-iv87@yandex.ru>
2023-09-22 10:00:30 +0500

    Правки скрипта построения образов

--- src/Docker/create-image.cmd
+++ src/Docker/create-image.cmd
@@ -1,1 +1,7 @@
+docker build --no-cache -f SQL\Dockerfile.PostgreSql -t reportstextclassifierai/postgre-sql ../SQL
+
 docker build --no-cache -f Dockerfile -t reportstextclassifierai/app ../..
+
+docker build --no-cache -f Dockerfile.ScikitLearnClassifier -t reportstextclassifierai/classifier ../..
+
+docker build --no-cache -f Dockerfile.PyTorchClassifier -t reportstextclassifierai/classifier2 ../..
